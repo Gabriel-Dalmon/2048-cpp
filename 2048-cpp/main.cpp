0c620de4772d24491796eaad536bd5a3dd2fc6b3
gdalmon <dalmgabi@gmail.com>
2023-10-31 12:09:17 +0100

    setup splitting main functions for threading

--- 2048-cpp/main.cpp
+++ 2048-cpp/main.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 #include <conio.h>
+#include <thread>
+#include <mutex>
+#include <chrono>
 #include "include/gamelogic/Board.h"
 #include "include/engine/SDLRenderer.h"
 #include "include/engine/SDLInputManager.h"
@@ -10,33 +13,49 @@
 #include <SDL_image.h>
 #include <SDL.h>
 
+std::mutex m;
+
+void renderLoop(bool* isRunning, Board* board) {
+    ConsoleRenderer* consoleRenderer = new ConsoleRenderer();
+    SDLRenderer* renderer = new SDLRenderer();
+    while (*isRunning) {
+        m.lock();
+        renderer->render(board);
+        consoleRenderer->render(board);
+        m.unlock();
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+    delete consoleRenderer;
+    delete renderer;
+}
+
+void logicLoop(bool* isRunning, Board* board) {
+    SDLInputManager* inputManager = new SDLInputManager();
+    int* inputs = inputManager->inputs;
+    while (*isRunning) {
+        inputManager->update();
+        m.lock();
+        board->update(inputs);
+        m.unlock();
+    }
+    delete inputManager;
+}
 
 int main(int argc, char* argv[1])
 {
     //srand(time(nullptr));
-    ConsoleRenderer* consoleRenderer = new ConsoleRenderer();
-    SDLRenderer* renderer = new SDLRenderer();
-    SDLInputManager* inputManager = new SDLInputManager();
+    
     Board* board = new Board(4);
 
-    int isPlaying = 1;
-    int* inputs = inputManager->inputs;
-    int slideMovement[2];
+    bool isRunning = true;  
 
-    while (isPlaying == 1) {
-        renderer->render(board);
-        if (inputs[0]) {
-            consoleRenderer->render(board);
-            inputs[0] = 0;
-        }
-        inputManager->update();
-        board->update(inputs);
-    }
+    std::thread logicLoopThread(logicLoop, &isRunning, board);
+    std::thread renderLoopThread(renderLoop, &isRunning, board);
+
+    logicLoopThread.join();
+    renderLoopThread.join();
     
-
-    delete inputManager;
     delete board;
-    delete renderer;
-    
+   
     return 0;
 }
